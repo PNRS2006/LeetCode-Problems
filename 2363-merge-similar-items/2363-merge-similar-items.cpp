5dc0b057fefb9e07d60f63ab7c8ca5828e8d315a
Pagadala Narasimha <pagadalanarasimha2610@gmail.com>
2026-09-03 10:43:03 +0530

    Time: 4 ms (69.59%), Space: 21.1 MB (44.98%) - LeetHub

--- 2363-merge-similar-items/2363-merge-similar-items.cpp
+++ 2363-merge-similar-items/2363-merge-similar-items.cpp
@@ -1,12 +1,11 @@
 class Solution {
 public:
     vector<vector<int>> mergeSimilarItems(vector<vector<int>>& i1, vector<vector<int>>& i2) {
-        unordered_map<int,int>m;
+        vector<int>m(1001,0);
         for(auto i : i1) m[i[0]]+=i[1];
         for(auto i : i2) m[i[0]]+=i[1];
         vector<vector<int>>v;
-        for(auto [k,l] : m) v.push_back({k,l});
-        sort(v.begin(),v.end());
+        for(int i=1;i<=1000;i++) if(m[i]>0) v.push_back({i,m[i]});
         return v;
     }
 };
